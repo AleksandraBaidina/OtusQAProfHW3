782b61199cf144827b6448f00c6259edeab402d9
Aleksandra Baidina <aleksandrabaidinas@gmail.com>
2023-02-13 20:13:31 +0500

    Specification

---
+++ src/main/java/mpack/Specification.java
@@ -0,0 +1,17 @@
+package mpack;
+
+import static io.restassured.http.ContentType.JSON;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+
+public class Specification {
+  private static final String BASE_URL = "https://petstore.swagger.io/v2";
+
+  protected static RequestSpecification getBaseSpec() {
+    return new RequestSpecBuilder()
+        .setContentType(JSON)
+        .setBaseUri(BASE_URL)
+        .build();
+  }
+}
