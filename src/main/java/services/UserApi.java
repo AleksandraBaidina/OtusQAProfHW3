9964f18aaaee4a69a20cae60733098568660d3f2
Aleksandra Baidina <aleksandrabaidinas@gmail.com>
2023-02-13 20:14:55 +0500

    UserApi service

---
+++ src/main/java/services/UserApi.java
@@ -0,0 +1,68 @@
+package services;
+
+import static io.restassured.RestAssured.given;
+
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import mpack.Specification;
+import mpack.User;
+
+public class UserApi extends Specification {
+
+  public ValidatableResponse logInUser(User user) {
+    return given()
+        .spec(getBaseSpec())
+        .queryParam("username", user.getUsername())
+        .queryParam("password", user.getPassword())
+        .when()
+        .get("/user/login")
+        .then();
+  }
+
+  public ValidatableResponse createUser(User user) {
+    return given()
+        .spec(getBaseSpec())
+        .body(user)
+        .when()
+        .post("/user")
+        .then();
+  }
+
+  public ValidatableResponse createUserWithoutBody() {
+    return given()
+        .spec(getBaseSpec())
+        .when()
+        .post("/user")
+        .then();
+  }
+
+
+  public User getUserByName(String userName) {
+
+    return given()
+        .spec(getBaseSpec())
+        .pathParam("username", userName)
+        .log().all()
+        .when()
+        .get("/user/{username}")
+        .then().statusCode(200).extract().body().as(User.class);
+  }
+
+  public ValidatableResponse getUserByNameResponse(String userName) {
+
+    return given()
+        .spec(getBaseSpec())
+        .pathParam("username", userName)
+        .log().all()
+        .when()
+        .get("/user/{username}")
+        .then();
+  }
+
+  public Response deleteUser(String userName) {
+    return given()
+        .spec(getBaseSpec())
+        .pathParam("username", userName)
+        .delete("/delete/{username}");
+  }
+}
